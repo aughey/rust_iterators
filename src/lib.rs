51a451ffb5c99e2d498c0f3117aa3a1559e05ac2
John Aughey <jha@aughey.com>
2024-01-08 22:08:57 +0000

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+use std::iter::from_fn;
+
+pub fn sine_wave(frequency: f64, sample_rate: f64, phase: f64) -> impl Iterator<Item = f64> {
+    let mut time = phase;
+    from_fn(move || {
+        let value = (time * frequency * 2.0 * std::f64::consts::PI).sin();
+        time += 1.0 / sample_rate;
+        Some(value)
+    })
+}
+
 pub struct SineWave {
     frequency: f64,
     sample_rate: f64,
@@ -58,6 +69,29 @@ impl Iterator for ZeroOne {
     }
 }
 
+// Rampes from 1 to 0 over the given number of seconds.
+pub fn ramp_down(seconds: f64, sample_rate: f64) -> impl Iterator<Item = f64> + Clone {
+    let step = 1.0 / (sample_rate * seconds);
+    let mut value = 1.0;
+    std::iter::from_fn(move || {
+        if value < 0.0 {
+            None
+        } else {
+            value -= step;
+            Some(value.abs())
+        }
+    })
+}
+
+pub fn zero() -> impl Iterator<Item = f64> {
+    std::iter::repeat(0.0)
+}
+
+pub fn one() -> impl Iterator<Item = f64> {
+    std::iter::repeat(1.0)
+}
+
+
 #[test]
 fn test_zeros() {
     let mut zeros = Zeros {};
@@ -97,4 +131,36 @@ fn test_sine_wave() {
     // now we go below
     // next sample should be less than zero
     assert!(sine_wave.next().unwrap() < 0.0);
+}
+
+#[test]
+fn test_both_sine_implementations_are_same() {
+    let mut sine_struct = SineWave::new(440.0, 44100.0);
+    let mut sine_fn = sine_wave(440.0, 44100.0, 0.0);
+    for _ in 0..100 {
+        assert_eq!(sine_struct.next(), sine_fn.next());
+    }
+}
+
+#[test]
+fn test_phase() {
+    let mut sine_struct = SineWave::new(440.0, 44100.0);
+    let mut sine_fn = sine_wave(440.0, 44100.0, 1.0);
+    for _ in 0..100 {
+        assert_ne!(sine_struct.next(), sine_fn.next());
+    }
+}
+
+#[test]
+fn test_ramp_down() {
+    let mut ramp = ramp_down(1.0, 44100.0);
+    // Should run for 44100 samples
+    let mut prev = ramp.next().unwrap();
+    for _ in 0..(44100-1) {
+        let val = ramp.next().unwrap();
+        assert!(val < prev);
+        prev = val;
+    }
+    assert!(prev < 0.00000000001); // last value is (nearly) zero
+    assert!(ramp.next().is_none());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+use std::{cell::RefCell, rc::Rc};
+
+use rust_iteartors::{ramp_down, zero, SineWave};
 fn main() {
-    println!("Hello, world!");
+    const SAMPLE_RATE: f64 = 8000.0;
+
+    let (mut samples, mut ping) = {
+        let sine = SineWave::new(440.0, SAMPLE_RATE);
+        let (ramp, ping) = CycleOnPing::new(ramp_down(1.0, SAMPLE_RATE), zero());
+
+        (sine.zip(ramp).map(|(s, a)| s * a), ping)
+    };
+
+    let loops = (SAMPLE_RATE * 2.0) as usize;
+
+    for sample in 0..=loops {
+        println!("{sample}: {}", samples.next().unwrap());
+        if sample == (SAMPLE_RATE * 1.5) as usize {
+            println!("RESET");
+            ping();
+        }
+    }
+}
+
+pub struct CycleOnPing<I1, I2> {
+    iter: I1,
+    iter_in_use: I1,
+    when_exhausted: I2,
+    reset: Rc<RefCell<bool>>,
+}
+
+impl<I1, I2> CycleOnPing<I1, I2>
+where
+    I1: Iterator<Item = f64> + Clone,
+    I2: Iterator<Item = f64>,
+{
+    pub fn new(iter: I1, when_exhausted: I2) -> (Self, impl FnMut()) {
+        let mut reset = Rc::new(RefCell::new(false));
+        (
+            Self {
+                reset: reset.clone(),
+                iter: iter.clone(),
+                iter_in_use: iter,
+                when_exhausted,
+            },
+            move || {
+                *reset.borrow_mut() = true;
+            },
+        )
+    }
+    pub fn ping(&mut self) {
+        self.iter_in_use = self.iter.clone();
+    }
+}
+impl<I1, I2> Iterator for CycleOnPing<I1, I2>
+where
+    I1: Iterator<Item = f64> + Clone,
+    I2: Iterator<Item = f64>,
+{
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.reset.replace(false) {
+            self.iter_in_use = self.iter.clone();
+        }
+        match self.iter_in_use.next() {
+            Some(value) => Some(value),
+            None => self.when_exhausted.next(),
+        }
+    }
 }
