743714d33e8a2cbb74d5f8161cdadcda42cc0526
John Aughey <jha@aughey.com>
2024-01-09 22:17:00 +0000

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -1,221 +1,84 @@
-struct Zeros {}
-impl Iterator for Zeros {
+pub struct Value {
+    value: i32,
+}
+impl Value {
+    pub fn new(value: i32) -> Value {
+        Value { value }
+    }
+}
+
+impl Iterator for Value {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(0)
+        Some(self.value)
     }
 }
 
-struct Ones {}
-impl Iterator for Ones {
+pub struct Counter {
+    count: i32,
+}
+impl Counter {
+    pub fn new(start: i32) -> Counter {
+        Counter { count: start }
+    }
+}
+
+impl Iterator for Counter {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(1)
+        let ret = self.count;
+        self.count = self.count.overflowing_add(1).0;
+        Some(ret)
     }
 }
 
-struct SingleNumbers {
-    ret: i32,
-}
-impl Iterator for SingleNumbers {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        Some(self.ret)
-    }
-}
-
-pub struct Incrementing {
-    ret: i32,
-}
-impl Incrementing {
-    pub fn new(start: i32) -> Self {
-        Self { ret: start }
-    }
-}
-impl Iterator for Incrementing {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let r = self.ret;
-        self.ret = self.ret.overflowing_add(1).0;
-        Some(r)
-    }
-}
-
-pub struct AllInVec {
-    v: Vec<i32>,
+pub struct VecIterator {
+    vec: Vec<i32>,
     index: usize,
 }
-impl AllInVec {
-    pub fn new(v: Vec<i32>) -> Self {
-        Self { v, index: 0 }
+impl VecIterator {
+    pub fn new(vec: Vec<i32>) -> VecIterator {
+        VecIterator { vec, index: 0 }
     }
 }
-impl Iterator for AllInVec {
+impl Iterator for VecIterator {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.v.len() {
+        if self.index >= self.vec.len() {
             None
         } else {
-            let i = self.index;
+            let ret = self.vec[self.index];
             self.index += 1;
-            Some(self.v[i])
-        }
-    }
-}
-
-pub struct AllInSlice<'a, T> {
-    v: &'a [T],
-    index: usize,
-}
-impl<'a, T> AllInSlice<'a, T> {
-    pub fn new(v: &'a [T]) -> Self {
-        Self { v, index: 0 }
-    }
-}
-impl<'a, T> Iterator for AllInSlice<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.v.len() {
-            None
-        } else {
-            let i = self.index;
-            self.index += 1;
-            Some(&self.v[i])
-        }
-    }
-}
-
-pub struct Take<I> {
-    iter: I,
-    count: usize,
-}
-impl<I> Take<I> {
-    pub fn new(iter: I, count: usize) -> Self {
-        Self { iter, count }
-    }
-}
-impl<I> Iterator for Take<I>
-where
-    I: Iterator<Item = i32>,
-{
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count > 0 {
-            self.count -= 1;
-            self.iter.next()
-        } else {
-            None
+            Some(ret)
         }
     }
 }
 
 #[test]
-fn test_zeros() {
-    let mut zeros = Zeros {};
-    assert_eq!(zeros.next(), Some(0));
-    assert_eq!(zeros.next(), Some(0));
-    assert_eq!(zeros.next(), Some(0));
-    assert_eq!(zeros.next(), Some(0));
+fn test_value() {
+    let mut z = Value::new(2);
+    assert_eq!(z.next(), Some(2));
+    assert_eq!(z.next(), Some(2));
+    assert_eq!(z.next(), Some(2));
 }
 
 #[test]
-fn test_ones() {
-    let mut ones = Ones {};
-    assert_eq!(ones.next(), Some(1));
-    assert_eq!(ones.next(), Some(1));
-    assert_eq!(ones.next(), Some(1));
-}
-
-#[test]
-fn test_numbers() {
-    let mut numbers = SingleNumbers { ret: 5 };
-    assert_eq!(numbers.next(), Some(5));
-    assert_eq!(numbers.next(), Some(5));
-    assert_eq!(numbers.next(), Some(5));
-
-    let mut numbers = SingleNumbers { ret: 8 };
-    assert_eq!(numbers.next(), Some(8));
-    assert_eq!(numbers.next(), Some(8));
-}
-
-#[test]
-fn test_incrementing() {
-    let mut incr = Incrementing { ret: 0 };
-    assert_eq!(incr.next(), Some(0));
-    assert_eq!(incr.next(), Some(1));
-    assert_eq!(incr.next(), Some(2));
-    assert_eq!(incr.next(), Some(3));
-
-    let mut incr = Incrementing {
-        ret: std::i32::MAX - 1,
-    };
-    assert_eq!(incr.next(), Some(std::i32::MAX - 1));
-    assert_eq!(incr.next(), Some(std::i32::MAX));
-    assert_eq!(incr.next(), Some(std::i32::MIN));
-}
-
-#[test]
-fn test_for_loop() {
-    let incr = Incrementing::new(0);
-
-    let mut expected = 0;
-    for value in incr {
-        assert_eq!(expected, value);
-        expected += 1;
-        if value == 10 {
-            break;
-        }
-    }
-}
-
-#[test]
-fn test_take() {
-    let mut take = Take {
-        iter: Zeros {},
-        count: 3,
-    };
-    assert!(take.next().is_some());
-    assert!(take.next().is_some());
-    assert!(take.next().is_some());
-    assert!(take.next().is_none());
+fn test_count() {
+    let mut z = Counter::new(2);
+    assert_eq!(z.next(), Some(2));
+    assert_eq!(z.next(), Some(3));
+    assert_eq!(z.next(), Some(4));
+    assert_eq!(z.next(), Some(5));
 }
 
 #[test]
 fn test_vec() {
-    let v = vec![1, 2, 3];
-
-    let mut iter = AllInVec::new(v);
-
-    assert_eq!(iter.next(), Some(1));
-    assert_eq!(iter.next(), Some(2));
-    assert_eq!(iter.next(), Some(3));
-    assert_eq!(iter.next(), None);
-}
-
-#[test]
-fn test_slice() {
-    let v = vec![1, 2, 3];
-
-    let mut iter = AllInSlice::new(v.as_slice());
-
-    assert_eq!(iter.next(), Some(&1));
-    assert_eq!(iter.next(), Some(&2));
-    assert_eq!(iter.next(), Some(&3));
-    assert_eq!(iter.next(), None);
-
-    let v = vec![1.0, 2.0, 3.0];
-
-    let mut iter = AllInSlice::new(v.as_slice());
-
-    assert_eq!(iter.next(), Some(&1.0));
-    assert_eq!(iter.next(), Some(&2.0));
-    assert_eq!(iter.next(), Some(&3.0));
-    assert_eq!(iter.next(), None);
+    let mut z = VecIterator::new(vec![1, 2, 3]);
+    assert_eq!(z.next(), Some(1));
+    assert_eq!(z.next(), Some(2));
+    assert_eq!(z.next(), Some(3));
+    assert_eq!(z.next(), None);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
-use rust_iterators::{Incrementing, Take};
+enum Kind {
+    Player,
+    Enemy,
+}
+
+struct Entity {
+    kind: Kind,
+    position: (i32, i32),
+}
 
 fn main() {
-    let values = Take::new(Incrementing::new(0), 10);
-    for value in values {
-        println!("value: {value}");
-    }
+    let entities = vec![
+        Entity {
+            kind: Kind::Player,
+            position: (0, 0),
+        },
+        Entity {
+            kind: Kind::Enemy,
+            position: (1, 1),
+        },
+        Entity {
+            kind: Kind::Enemy,
+            position: (2, 2),
+        },
+    ];
+
+    let enemies = entities.iter().filter(|e| matches!(e.kind, Kind::Enemy));
+    let players = entities.iter().filter(|e| matches!(e.kind, Kind::Player));
+
+    let _closest_enemy = closest_entity(enemies, (0, 0));
+    let _closest_player = closest_entity(players, (0, 0));
+}
+
+fn distance_to_entity(entity: &Entity, position: (i32, i32)) -> i32 {
+    (entity.position.0 - position.0).abs() + (entity.position.1 - position.1).abs()
+}
+
+fn closest_entity<'a>(
+    entities: impl Iterator<Item = &'a Entity>,
+    position: (i32, i32),
+) -> Option<&'a Entity> {
+    entities.min_by_key(|e| distance_to_entity(e, position))
 }
